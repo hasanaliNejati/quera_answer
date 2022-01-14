0b1b63cf7b01e3e011430e8541797ef2ff300162
hasanali <Irmanoli81uf@gmail.com>
2022-01-14 03:44:28 -0800

    add 8358.cs

---
+++ C#/8358.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.ReadLine();
+        string[] inputString = Console.ReadLine().Split(' ');
+
+        long output = 0;
+        for (int i = 0; i < inputString.Length; i++)
+        {
+
+            if (int.Parse(inputString[i]) < 0)
+            {
+                output += inputString.Length-1;
+            }
+
+        }
+        Console.WriteLine(output);
+
+    }
+}
