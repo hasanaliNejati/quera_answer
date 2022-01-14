11a4f8968e3b2836c21ac1bf48aa0d6fcb4ff3c3
hasanali <Irmanoli81uf@gmail.com>
2022-01-14 01:33:17 -0800

    add 10326.cs

---
+++ C#/10326.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputString = Console.ReadLine().Split(' ');
+        int[] inputInt = new int[] { int.Parse(inputString[0]), int.Parse(inputString[2]) };
+        int[] outputInt = new int[4];
+        for (int i = 0; ; i++)
+        {
+            inputInt[i % 2]--;
+            outputInt[i % 4]++;
+            if (inputInt[i % 2] <= 0)
+                break;
+        }
+        Console.WriteLine(outputInt[0] + " " + outputInt[1] + " " + outputInt[2] + " " + outputInt[3]);
+
+    }
+}
