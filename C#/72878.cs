aa1e37efa65f32e44662f5fbaa6ec85723943fed
hasanali <Irmanoli81uf@gmail.com>
2022-01-14 19:17:27 -0800

    add 72878.cs

---
+++ C#/72878.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int time = int.Parse(input[0]);
+        int a = int.Parse(input[1]);
+        int b = int.Parse(input[2]);
+
+        int ar_ar = 0;
+        int ma_ma = 0;
+
+        while(time > 0)
+        {
+            //ar_ar
+            if (time > 0)
+            {
+                time--;
+                ar_ar++;
+                time -= a;
+            }
+            //ma_ma
+            if (time > 0)
+            {
+                time--;
+                ma_ma++;
+                time -= b;
+            }
+        }
+
+        Console.WriteLine(ar_ar + " " + ma_ma);
+    }
+}
