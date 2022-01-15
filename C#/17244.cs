6f71127c15a896c26e9cca738a502f6955b7431d
hasanali <Irmanoli81uf@gmail.com>
2022-01-14 18:57:41 -0800

    add 17244.cs

---
+++ C#/17244.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(factorial(int.Parse(Console.ReadLine())));
+    }
+
+    static int factorial(int num)
+    {
+        return (num == 1) ? 1 : num + factorial(num - 1);
+    }
+}
