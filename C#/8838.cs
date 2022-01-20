2f3046c32ada41fca6d452a5705b1912cb48fdb8
hasanali <Irmanoli81uf@gmail.com>
2022-01-20 12:38:45 -0800

    add 8838.cs

---
+++ C#/8838.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        for (int i = 0; i < int.Parse(input[0]); i++)
+        {
+            Console.Write("copy of ");
+        }
+        Console.WriteLine(input[1]);
+    }
+
+}
