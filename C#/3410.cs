5b805f39409199d86095b965b245389e167130fc
hasanali <Irmanoli81uf@gmail.com>
2022-01-20 23:00:52 -0800

    add 3410.cs

---
+++ C#/3410.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        long hight = int.Parse(Console.ReadLine());
+        drowKhaiam(new long[] { 1 }, hight);
+    }
+
+    static void drowKhaiam(long[] row, long hight)
+    {
+        write(row);
+
+        long[] nextRow = new long[row.Length+1];
+        nextRow[0] = 1;
+        for (int i = 1;i < nextRow.Length-1;i++)
+        {
+            nextRow[i] = row[i-1]+row[i];
+        }
+        nextRow[nextRow.Length-1] = 1;
+
+        if (row.Length >= hight)
+            return;
+
+        drowKhaiam(nextRow, hight);
+
+
+    }
+
+    static void write(long[] row)
+    {
+        for (int i = 0; i < row.Length; i++)
+            Console.Write(row[i] + " ");
+        Console.WriteLine();
+
+    }
+
+}
